6c7e4a9320c8cbd1778a77f4ca1bee4c2bfb47ff
Frederick Lu <lu.fredmac4@gmail.com>
2023-06-12 13:51:52 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "plant-finder-41423",
   url: "https://plant-finder-41423.botics.co",
   api: "https://plant-finder-41423.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
